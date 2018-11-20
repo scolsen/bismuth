00a5e20ef696d9052509e22f05a01ef96a3c2c6a
Scott Olsen <scg.olsen@gmail.com>
2018-11-20 17:35:50 -0500

    Add simple readline loop.

--- src/bismuth.cr
+++ src/bismuth.cr
@@ -1,6 +1,21 @@
 # TODO: Write documentation for `Bismuth`
+
+require "readline"
+
 module Bismuth
   VERSION = "0.1.0"
+  extend self
+
+  @@command : String | Nil
 
   # TODO: Put your code here
+  def start
+    loop do
+      @@command = Readline.readline("bismuth> ")
+      break if @@command.nil?
+      puts @@command
+    end
+  end
 end
+
+Bismuth.start
