c6e5a9c27a7a8125da4d3136681964782895b0e9
Scott Olsen <scg.olsen@gmail.com>
2018-11-26 17:14:59 -0500

    Begin to abstract over FD couplings.

--- src/bismuth/pipeline.cr
+++ src/bismuth/pipeline.cr
@@ -1,4 +1,46 @@
 module Bismuth::Pipeline
+  abstract class Coupling
+    getter source, dest
+    setter origin
+
+    def initialize()
+      @source, @dest = IO.pipe
+    end
+
+    def connect(coupling : Coupling)
+      plunge(input: @origin, output: @dest)
+      @dest.close
+      coupling.origin = @source
+    end
+
+    abstract def plunge(input : IO::FileDescriptor, output : IO::FileDescriptor)
+    end
+  end
+ 
+  class ProcessCoupling < Coupling
+    def initialize(command : Command)
+      super
+      @command   = command[0]
+      @arguments = command[1]
+    end
+    
+    def plunge(input : IO::FileDescriptor, output : IO::FileDescriptor)
+      Process.run(@command, @arguments, input: input, output: output)
+    end
+  end
+
+  class BuiltinCoupling < Coupling
+    def initialize(command : Command)
+      super
+      @command   = command[0]
+      @arguments = command[1]
+    end
+
+    def plunge(input, output)
+      Builtins.run(@command, @arguments, input: input, output: output)
+    end
+  end
+
   class Pipeline
     getter endpoint 
     
